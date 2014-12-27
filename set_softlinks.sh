e715de9976ace671b248c6255a5086df0af17dd5
phil <philipp.hitzler@bettermarks.de>
2014-12-27 22:20:30 +0100

    set pathes explicitly

--- set_softlinks.sh
+++ set_softlinks.sh
@@ -1,33 +1,32 @@
 #!/bin/bash
 
 DOTFILES=dotfiles
-cd $HOME
 
 # bash
 echo bash
-ln -s $DOTFILES/bash/.bashrc
+ln -s $HOME/$DOTFILES/bash/.bashrc $HOME/.bashrc
 
 # git
 echo git
-ln -s $DOTFILES/git/.gitconfig
+ln -s $HOME/$DOTFILES/git/.gitconfig $HOME/.gitconfig
 
 # i3
 echo i3
-mkdir -p .i3
-ln -s $DOTFILES/i3/config .i3/config
+mkdir -p $HOME/.i3
+ln -s $HOME/$DOTFILES/i3/config $HOME/.i3/config
 
 # vim
 echo vim
-ln -s $DOTFILES/vim/vimrc .vimrc
+ln -s $HOME/$DOTFILES/vim/vimrc $HOME/.vimrc
 
 # X11
 echo X11
-ln -s $DOTFILES/X11/.xinitrc
-ln -s $DOTFILES/X11/.Xresources
-ln -s $DOTFILES/X11/.Xdefaults
+ln -s $HOME/$DOTFILES/X11/.xinitrc $HOME/.xinitrc
+ln -s $HOME/$DOTFILES/X11/.Xresources $HOME/.Xresources
+ln -s $HOME/$DOTFILES/X11/.Xdefaults $HOME/.Xdefaults
 
 # zsh
 echo zsh
-ln -s $DOTFILES/zsh/.zshrc
+ln -s $HOME/$DOTFILES/zsh/.zshrc $HOME/.zshrc
 
 
