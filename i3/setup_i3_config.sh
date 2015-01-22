b5f7e7a6c1e2ef2d2531dfa2bfa02e1539d343d4
phil <philipp.hitzler@bettermarks.de>
2015-01-22 22:35:02 +0100

    setup i3 config file

---
+++ i3/setup_i3_config.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+
+HOST_CONF=$HOME/dotfiles/i3/config.host.$HOSTNAME
+
+if [ -f $HOST_CONF ]; then
+    echo "Found config for host"
+    cat $HOME/dotfiles/i3/config.base \
+        $HOST_CONF > $HOME/.config/i3/config
+else
+    echo "No config for host"
+    cat $HOME/dotfiles/i3/config.base > $HOME/.config/i3/config
+fi
+
