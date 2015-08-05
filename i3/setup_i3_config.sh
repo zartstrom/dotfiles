49a3cf407ca91a15c6096cf65c707dba2fb69a32
zartstrom <phj.hitzler@gmail.com>
2015-08-05 10:30:02 +0200

    give firefox and chrome their own workspaces

--- i3/setup_i3_config.sh
+++ i3/setup_i3_config.sh
@@ -4,7 +4,7 @@
 HOST_CONF=$HOME/dotfiles/i3/config.host.$HOSTNAME
 
 if [ -f $HOST_CONF ]; then
-    echo "Found config for host"
+    echo "Found config for host \"$HOSTNAME\""
     cat $HOME/dotfiles/i3/config.base \
         $HOST_CONF > $HOME/.config/i3/config
 else
