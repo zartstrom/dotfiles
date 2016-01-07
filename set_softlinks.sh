b616fcfa77c1e70d8983d6090a723dc03f5495fe
zartstrom <phj.hitzler@gmail.com>
2016-01-07 13:32:54 +0100

    added imwheel

--- set_softlinks.sh
+++ set_softlinks.sh
@@ -1,59 +1,68 @@
 #!/bin/bash
 
+
+function display_header {
+    echo
+    echo "--- $1 ---"
+}
+
+
 DOTFILES=$HOME/dotfiles
 cd $HOME
 
 # bash
-echo bash
-ln -s $DOTFILES/bash/.bashrc $HOME/.bashrc
-ln -s $DOTFILES/bash/.bash_aliases $HOME/.bash_aliases
+display_header bash
+ln -sv $DOTFILES/bash/.bashrc $HOME/.bashrc
+ln -sv $DOTFILES/bash/.bash_aliases $HOME/.bash_aliases
 
 # firefox (no Desktop folder please)
-echo firefox
+display_header firefox
 mkdir -p $HOME/.config
-ln -s $DOTFILES/firefox/user-dirs.dirs $HOME/.config/user-dirs.dirs
+ln -sv $DOTFILES/firefox/user-dirs.dirs $HOME/.config/user-dirs.dirs
 
 # git
-echo git
-ln -s $DOTFILES/git/.gitconfig $HOME/.gitconfig
+display_header git
+ln -sv $DOTFILES/git/.gitconfig $HOME/.gitconfig
 
 # i3
-echo i3
+display_header i3
 mkdir -p $HOME/.config/i3
 # no I do it with setup_i3_config.sh which is sourced in .xinitc
-# ln -s $HOME/$DOTFILES/i3/config $HOME/.config/i3/config
+# ln -sv $HOME/$DOTFILES/i3/config $HOME/.config/i3/config
 mkdir -p $HOME/.config/i3status
-ln -s $DOTFILES/i3/i3status.conf $HOME/.config/i3status/config
+ln -sv $DOTFILES/i3/i3status.conf $HOME/.config/i3status/config
 
 mkdir -p $HOME/scripts
-ln -s $DOTFILES/i3/i3exit.sh $HOME/scripts/i3exit.sh
-ln -s $DOTFILES/i3/lock.sh $HOME/scripts/lock.sh
+ln -sv $DOTFILES/i3/i3exit.sh $HOME/scripts/i3exit.sh
+ln -sv $DOTFILES/i3/lock.sh $HOME/scripts/lock.sh
 
 # pylint
-echo pylint
-ln -s $DOTFILES/pylint/pylintrc $HOME/.pylintrc
+display_header pylint
+ln -sv $DOTFILES/pylint/pylintrc $HOME/.pylintrc
 
 # scripts
-echo scripts
+display_header scripts
 mkdir -p $HOME/scripts
-ln -s $DOTFILES/scripts/touchpad_toggle.sh $HOME/scripts/touchpad_toggle.sh 
+ln -sv $DOTFILES/scripts/touchpad_toggle.sh $HOME/scripts/touchpad_toggle.sh 
 
 # vim
-echo vim
+display_header vim
 mkdir -p $HOME/.vim
 mkdir -p $HOME/.vimswap
-ln -s $DOTFILES/vim/vimrc $HOME/.vimrc
+ln -sv $DOTFILES/vim/vimrc $HOME/.vimrc
 
 # X11
-echo X11
-ln -s $DOTFILES/X11/.xinitrc $HOME/.xinitrc
-ln -s $DOTFILES/X11/.xprofile $HOME/.xprofile
-ln -s $DOTFILES/X11/.Xresources $HOME/.Xresources
-ln -s $DOTFILES/X11/.Xdefaults $HOME/.Xdefaults
+display_header X11
+ln -sv $DOTFILES/X11/.xinitrc $HOME/.xinitrc
+ln -sv $DOTFILES/X11/.xprofile $HOME/.xprofile
+ln -sv $DOTFILES/X11/.Xresources $HOME/.Xresources
+ln -sv $DOTFILES/X11/.Xdefaults $HOME/.Xdefaults
+# mouse config:
+ln -sv $DOTFILES/X11/.imwheelrc $HOME/.imwheelrc
 
 # zsh
-echo zsh
-ln -s $DOTFILES/zsh/.zshrc $HOME/.zshrc
+display_header zsh
+ln -sv $DOTFILES/zsh/.zshrc $HOME/.zshrc
 mkdir -p $HOME/.oh-my-zsh/themes
-ln -s $DOTFILES/zsh/af-magic-light.zsh-theme $HOME/.oh-my-zsh/themes
+ln -sv $DOTFILES/zsh/af-magic-light.zsh-theme $HOME/.oh-my-zsh/themes
 
