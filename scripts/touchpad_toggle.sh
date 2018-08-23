875de3aadbf11123e764386631295b6ec2bd2a50
zartstrom <phj.hitzler@gmail.com>
2018-08-23 22:15:51 +0200

    updated touchpad name

--- scripts/touchpad_toggle.sh
+++ scripts/touchpad_toggle.sh
@@ -3,61 +3,63 @@
 # see https://wiki.archlinux.org/index.php/Touchpad_Synaptics
 
 # the name of the touchpad device may vary
-DEVICE_NAME_LENOVO="Synaptics Touchpad"
+DEVICE_NAME_LENOVO="Synaptics tm2964-001"
 DEVICE_NAME_DELL="GlidePoint"
 
+#DEVICE_REGEX="($DEVICE_NAME_LENOVO|$DEVICE_NAME_LENOVO_NEW|$DEVICE_NAME_DELL)"
 DEVICE_REGEX="($DEVICE_NAME_LENOVO|$DEVICE_NAME_DELL)"
 
 
 function get_touchpad_id {
     declare -i ID
-    ID=`xinput list | grep -Eio "$DEVICE_REGEX\s*id\=[0-9]{1,2}" | grep -Eo '[0-9]{1,2}'`
-    echo $ID
+    ID=$(xinput list | grep -Eio "$DEVICE_REGEX\s*id\=[0-9]{1,2}" | grep -Eo '[0-9]{1,2}$')
+    echo "$ID"
 }
 
 function get_touchpad_state {
     declare -i STATE
-    STATE=`xinput list-props $1|grep 'Device Enabled'|awk '{print $4}'`
-    echo $STATE
+    STATE=$(xinput list-props "$1"|grep 'Device Enabled'|awk '{print $4}')
+    echo "$STATE"
 }
 
 function touchpad_enable {
-    xinput enable $1
+    xinput enable "$1"
     echo "Touchpad enabled."
     # notify-send 'Touchpad' 'Enabled' -i /usr/share/icons/Adwaita/48x48/devices/input-touchpad.png
 }
 
 function touchpad_disable {
-    xinput disable $1
+    xinput disable "$1"
     echo "Touchpad disabled."
     # notify-send 'Touchpad' 'Disabled' -i /usr/share/icons/Adwaita/48x48/devices/input-touchpad.png
 }
 
 function toggle_switch {
     ID=$(get_touchpad_id)
-    STATE=$(get_touchpad_state $ID)
-    if [ $STATE -eq 1 ]
+    echo "$ID"
+    STATE=$(get_touchpad_state "$ID")
+    if [ "$STATE" -eq 1 ]
     then
-        touchpad_disable $ID
+        touchpad_disable "$ID"
     else
-        touchpad_enable $ID
+        touchpad_enable "$ID"
     fi
 }
 
 function toggle_off {
     ID=$(get_touchpad_id)
-    touchpad_disable $ID
+    touchpad_disable "$ID"
 }
 
 function toggle_on {
     ID=$(get_touchpad_id)
-    touchpad_enable $ID
+    touchpad_enable "$ID"
 }
 
 function toggle_status {
     ID=$(get_touchpad_id)
-    STATE=$(get_touchpad_state $ID)
-    if [ $STATE -eq 1 ]
+    STATE=$(get_touchpad_state "$ID")
+    if [ "$STATE" -eq 1 ]
     then
         echo "Touchpad is enabled."
     else
