16fc5b0613d4b8f10a1ff7dfc337af54fd72fef9
zartstrom <phj.hitzler@gmail.com>
2018-08-23 22:16:53 +0200

    removed comments

--- scripts/touchpad_toggle.sh
+++ scripts/touchpad_toggle.sh
@@ -6,7 +6,6 @@
 DEVICE_NAME_LENOVO="Synaptics tm2964-001"
 DEVICE_NAME_DELL="GlidePoint"
 
-#DEVICE_REGEX="($DEVICE_NAME_LENOVO|$DEVICE_NAME_LENOVO_NEW|$DEVICE_NAME_DELL)"
 DEVICE_REGEX="($DEVICE_NAME_LENOVO|$DEVICE_NAME_DELL)"
 
 
@@ -25,18 +24,15 @@ function get_touchpad_state {
 function touchpad_enable {
     xinput enable "$1"
     echo "Touchpad enabled."
-    # notify-send 'Touchpad' 'Enabled' -i /usr/share/icons/Adwaita/48x48/devices/input-touchpad.png
 }
 
 function touchpad_disable {
     xinput disable "$1"
     echo "Touchpad disabled."
-    # notify-send 'Touchpad' 'Disabled' -i /usr/share/icons/Adwaita/48x48/devices/input-touchpad.png
 }
 
 function toggle_switch {
     ID=$(get_touchpad_id)
-    echo "$ID"
     STATE=$(get_touchpad_state "$ID")
     if [ "$STATE" -eq 1 ]
     then
