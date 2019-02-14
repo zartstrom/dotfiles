0117d82fbde2e5135f661bac20a27b856e7ef7c0
zartstrom <phj.hitzler@gmail.com>
2019-02-14 23:24:03 +0100

    use grim instead of scrot

--- i3/lock.sh
+++ i3/lock.sh
@@ -2,7 +2,7 @@
 
 LOCK_IMAGE=$HOME/mega/docs/pictures/lock-symbol.png
 
-scrot /tmp/screen.png
+grim /tmp/screen.png
 convert /tmp/screen.png -scale 10% -scale 1000% /tmp/screen.png
 #[[ -f ${LOCK_IMGE} ]] && convert /tmp/screen.png ${LOCK_IMAGE} -gravity center -composite -matte /tmp/screen.png
 i3lock -u -i /tmp/screen.png
