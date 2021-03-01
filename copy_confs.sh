3359efb20b7d224b5d143b408eba3b869517dc3e
Philipp Hitzler <philipp.hitzler@idealo.de>
2021-03-01 15:17:49 +0100

    updated screenshot script

--- copy_confs.sh
+++ copy_confs.sh
@@ -8,9 +8,9 @@ DOTFILES=/home/phil/dotfiles
 
 
 cp $DOTFILES/conf/pacman.conf /etc/pacman.conf
-cp $DOTFILES/conf/slim.conf /etc/slim.conf
+# cp $DOTFILES/conf/slim.conf /etc/slim.conf
 cp $DOTFILES/conf/locale.conf /etc/locale.conf
-cp $DOTFILES/conf/10-keyboard.conf /etc/X11/xorg.conf.d/10-keyboard.conf
+# cp $DOTFILES/conf/10-keyboard.conf /etc/X11/xorg.conf.d/10-keyboard.conf
 cp $DOTFILES/conf/vconsole.conf /etc/vconsole.conf
 cp $DOTFILES/conf/main.conf /etc/bluetooth/main.conf
 cp $DOTFILES/conf/environment /etc/environment
--- scripts/screenshot.sh
+++ scripts/screenshot.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 datetime=$(date +'%Y-%m-%d_%H%M%S')
-target_file=$(xdg-user-dir PICTURES)/screenshot_${datetime}.png
+mkdir -p $(xdg-user-dir PICTURES)/screenshots
+target_file=$(xdg-user-dir PICTURES)/screenshots/screenshot_${datetime}.png
 #grim -o $(swaymsg -t get_outputs | jq -r '.[] | select(.focused) | .name') $target_file
 grim -g "$(slurp)" $target_file
 
