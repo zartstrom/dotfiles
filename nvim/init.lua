dd7ce2d6e258510110fa957514c909114ef7c90b
Philipp Hitzler <philipp.hitzler@idealo.de>
2021-07-10 02:47:02 +0200

    added space to tabline separator

--- nvim/init.lua
+++ nvim/init.lua
@@ -135,7 +135,7 @@ vim.g.airline_left_sep = ''
 vim.g.airline_left_alt_sep = ''
 vim.g.airline_right_sep = ''
 vim.g.airline_right_alt_sep = ''
-vim.g["airline#extensions#tabline#left_sep"] = ''
+vim.g["airline#extensions#tabline#left_sep"] = ' '
 vim.g["airline#extensions#tabline#left_alt_sep"] = ''
 vim.g["airline#extensions#keymap#enabled"] = 0
 
