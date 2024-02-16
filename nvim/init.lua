06a0fbc679c34134a286876cc1bae3eaeec4fbe5
Philipp Hitzler <philipp.hitzler@idealo.de>
2024-02-16 11:48:54 +0100

    renamed windows

--- nvim/init.lua
+++ nvim/init.lua
@@ -93,10 +93,8 @@ require("lazy").setup(
             "lukas-reineke/indent-blankline.nvim",
             -- Enable `lukas-reineke/indent-blankline.nvim`
             -- See `:help indent_blankline.txt`
-            opts = {
-                char = "┊",
-                show_trailing_blankline_indent = false
-            }
+            main = "ibl",
+            opts = {}
         },
         -- "gc" to comment visual regions/lines
         {"numToStr/Comment.nvim", opts = {}},
